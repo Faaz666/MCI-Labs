9904b0a28f6c8b830e08c21eec01215bfcf9e8d1
Lab T1 <fs09911@st.habib.edu.pk>
2026-01-20 17:14:35 +0500

    task0: basic UART hello

--- Lab2/Core/Src/main.c
+++ Lab2/Core/Src/main.c
@@ -110,7 +110,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    char msg[] = "Hello, World!\r\n";
+    HAL_UART_Transmit(&huart2, (uint8_t *)msg, sizeof(msg) - 1U, HAL_MAX_DELAY);
+    HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
